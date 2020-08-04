dd24964965a4bc40e2c002fe18a95667e194152a
Zoe Zhang <jin0925aki@gmail.com>
2020-08-04 11:57:18 +1000

    Edit MVC

--- app/resolvers/User.resolver.js
+++
@@ -1,9 +0,0 @@
-const Project = require('../models/Project.model.js');
-const { promisify } = require('../helpers.js');
-
-
-const resolvers = {
-    projects: user => promisify(Project.find({ members: { $elemMatch: { $eq: user.id } } })),
-};
-
-module.exports = resolvers;
